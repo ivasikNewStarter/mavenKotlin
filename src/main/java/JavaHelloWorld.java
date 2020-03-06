6df1b74192371ff3194fbd54bd0cfcfc6383c033
Iwan Szmigelski <ivan.shmihelskyy@thomsonreuters.com>
2020-03-06 11:56:22 +0100

    updates of Kotlin class

---
+++ src/main/java/JavaHelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by u0139221 on 3/6/2020.
+ */
+public class JavaHelloWorld {
+    public String hello () {
+        return "testing maven with Kotlin";
+    }
+}
---
+++ src/main/kotlin/Hello.kt
@@ -0,0 +1,10 @@
+/**
+ * Created by  on 3/6/2020.
+ */
+
+fun main (aeg : Array<String>){
+    val hi = JavaHelloWorld()
+    println(hi.hello())
+}
+
+
